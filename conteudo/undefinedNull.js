f5f090a1c3032609b94fee87fe542452d0c14abf
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-09-24 16:33:39 -0300

    insere exemplo do undefined e do null

---
+++ conteudo/undefinedNull.js
@@ -0,0 +1,12 @@
+/*
+
+    undefined
+        não existe
+
+    null
+        existe mas tem nada dentro dele
+        objeto que possui nada dentro
+
+*/
+
+console.log(undefined === null) // exemplo que mostra que são diferentes
