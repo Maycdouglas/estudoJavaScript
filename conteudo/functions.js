850655f5f80d72e567f5832852c33ce5d0c69612
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-09-25 15:09:05 -0300

    insere exemplos com funcoes

---
+++ conteudo/functions.js
@@ -0,0 +1,49 @@
+/*
+
+    FUNÇÕES são um tipo de dado estrutural
+
+*/
+
+
+// declaração da função
+function createPhrases() {
+    console.log('Nada está tão ruim que não possa piorar')
+    console.log('Acorde, é mais um dia para o fracasso')
+    console.log('Não existe prazer maior do que desistir')
+}
+
+//executa a função
+createPhrases()
+
+//function expression or function anonymous
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2,3)
+
+//function hoisting
+sayMyName()
+
+function sayMyName() {
+    console.log('Maycon')
+}
+
+//arrow function
+
+const sayMyName = (name) => {
+    console.log(name)
+}
+
+sayMyName('Maycon')
+
+//callback function
+function digaMeuNome(nome) {
+    nome()
+}
+
+digaMeuNome(
+    () => {
+        console.log("Estou em uma callback")
+    }
+)
--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 /*
 
+    Function() constructor
+
+    * expressão new
+    * criar um novo objeto
+    * this keyword
 
 */
 
+function Person(name) {
+    this.name = name
+    this.walk = function() {
+        return this.name + " está andando"
+    }
+}
+const maycon = new Person("Maycon")
+console.log(maycon.walk())
