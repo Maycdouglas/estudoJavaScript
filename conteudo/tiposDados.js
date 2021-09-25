dff588ce7b7e398ed9797a4fdeb34b135de54b70
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-09-25 10:26:18 -0300

    insere exemplo de variaveis e junta todos os tipos de dados em um arquivo

---
+++ conteudo/tiposDados.js
@@ -0,0 +1,111 @@
+/*
+    String é uma cadeia de caracteres
+
+    Deve estar entre uma das seguintes opções:
+    "" -> aspas duplas
+    '' -> aspas simples
+    `` -> template literals ou template strings
+        - essa última opção permite multíplas linhas e
+        também que insira expressões de linguagem (isso se chama interpolação)
+*/
+
+console.log("Exemplo com aspas duplas")
+console.log('Exemplo com aspas simples')
+console.log(`Exemplo com template literals`)
+console.log("Exemplo para usar as aspas simples ' ' ' ' ")
+console.log('Exemplo para usar as aspas duplas " " " " ')
+console.log(`Exemplo de 
+multiplas 
+linhas`)
+console.log(`Exemplo de interpolação: ${1 + 1}`)
+
+// ============================================================ //
+
+/*
+
+    Number é um tipo de dado para os números
+
+    Os tipos que o number pode ter:
+
+    10 -> Inteiros
+    10.5 -> Reais ou Float
+    NaN -> Not a Number
+    Infinity -> Infinito
+
+*/
+
+console.log(10) // exemplo com número inteiro
+console.log(10.5) // exemplo com número real
+console.log(10 / "String") // exemplo com Not a Number
+console.log(Infinity) // exemplo com Infinito 
+
+// ============================================================ //
+
+/* 
+    Boolean é um tipo de dado que tem apenas 2 valores
+    
+    O nome é para homenagear o criador, George Boole
+
+    São eles:
+    - true
+    - false
+
+*/
+
+console.log(true) //exemplo com true
+console.log(false) //exemplo com false
+
+// ============================================================ //
+
+/*
+
+    Object é um tipo de dado estrutural, ou seja, cria uma estrutura
+
+    Possui propriedades -> Atributos
+    Possui funcionalidades -> Métodos
+
+    { propriedade: "valor" }
+
+*/
+
+console.log({
+    name: "Maycon",
+    idade: 21,
+    vivo: true,
+    andar: function() {
+        console.log('andar')
+    }
+})
+
+// ============================================================ //
+
+/*
+
+    Array (Vetores) é um agrupamento de dados, uma lista
+
+    [Maycon, 21]
+
+*/
+
+console.log([
+    "Maycon", 
+    21, 
+    true
+])
+
+// ============================================================ //
+
+/*
+
+    undefined
+        não existe
+
+    null
+        existe mas tem nada dentro dele
+        objeto que possui nada dentro
+
+*/
+
+console.log(undefined === null) // exemplo que mostra que são diferentes
+
+// ============================================================ //
---
+++ conteudo/variaveis.js
@@ -0,0 +1,30 @@
+/*
+
+    Variáveis em JavaScript podem ser:
+
+        * var
+            é global
+        * let
+            só funciona no escopo que foi criada
+        * const
+            não pode ser alterado durante a execução da aplicação
+            só funciona no escopo que foi criada
+
+    O JavaScript é uma linguagem fracamente tipada, então o tipo da variável é de acordo com a sua inicialização
+*/
+
+var cor = "Azul"
+cor = "Vermelho"
+console.log(cor)
+
+let tamanho = "Grande"
+tamanho = "Pequeno"
+console.log(tamanho)
+
+const estrela = "Sol"
+console.log(estrela)
+
+let variavel = "Palavra" //inicializa a variavel como tipo String
+console.log(typeof variavel)
+variavel = 24 //muda o tipo da variavel para Number
+console.log(typeof variavel)
