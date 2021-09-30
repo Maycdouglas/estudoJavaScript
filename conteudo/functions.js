40f046be9bb1d493ca110cec412d7c627f7d770a
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-09-30 14:09:19 -0300

    insere a criacao da funcao foreach

--- conteudo/functions.js
+++ conteudo/functions.js
@@ -113,3 +113,16 @@ aprovados.forEach(nome => console.log(nome))
 
 const exibirAprovados = aprovado => console.log(aprovado)
 aprovados.forEach(exibirAprovados)
+
+//criando um forEach
+Array.prototype.forEach2 = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this)
+    }
+}
+
+const aprovados = ['Ana', 'Bia', 'Manoel', "Zoo", 'Pedro']
+
+aprovados.forEach2(function(nome, indice) {
+    console.log(`${indice + 1}) ${nome}`)
+})
