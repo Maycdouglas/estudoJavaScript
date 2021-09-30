ce2e1e022523e820ad0e6fceda05f25aa6093462
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-09-30 14:56:30 -0300

    insere exemplo com funcao filter

--- conteudo/functions.js
+++ conteudo/functions.js
@@ -156,3 +156,17 @@ const apenasPreco = produto => produto.preco
 
 const resultado2 = carrinho.map(paraObjeto).map(apenasPreco)
 console.log(resultado2)
+
+
+//Função Filter
+
+const produtos = [
+    {nome: 'Notebook', preco: 2499, fragil: true},
+    {nome: 'iPad Pro', preco: 4199, fragil: true},
+    {nome: 'Copo de Vidro', preco: 12.49, fragil: true},
+    {nome: 'Copo de Plástico', preco: 18.99, fragil: false}
+]
+
+console.log(produtos.filter(function(p) {
+    return p.preco > 500 && p.fragil
+}))
