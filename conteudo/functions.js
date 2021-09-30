77c0a2b994105bc9599cab7d046543595bd36513
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-09-30 16:53:20 -0300

    Insere exemplo com a funcao flatmap

--- conteudo/functions.js
+++ conteudo/functions.js
@@ -194,3 +194,38 @@ const filhas = ["Maisa", "Joaquina"]
 const filhos = ["João", "Pedro"]
 const todos = filhas.concat(filhos,"Posso passar quantos parametros eu quiser")
 console.log(todos, filhas, filhos)
+
+//Função FlatMap
+
+const escola = [
+    {
+        nome: 'Turma M1',
+        alunos: [{
+            nome: "Gustavo",
+            nota: 8.1
+        }, {
+            nome: 'Ana',
+            nota: 9.3
+        }]
+    },
+    {
+        nome: 'Turma M2',
+        alunos: [{
+            nome: "Rebeca",
+            nota: 8.9
+        },{
+            nome: "Roberto",
+            nota: 7.3
+        }]
+    }
+]
+
+const getNotaDoAluno = aluno => aluno.nota
+const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
+
+const notas1 = escola.map(getNotasDaTurma)
+console.log(notas1)
+
+const notas2 = escola.flatMap(getNotasDaTurma)
+
+console.log(notas2)
