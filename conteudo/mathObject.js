ddc7c4239e30a76e65ce8c28d48315a2608a1d2d
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2022-03-01 13:08:39 -0300

    Insere propriedades do objeto Math

---
+++ conteudo/mathObject.js
@@ -0,0 +1,17 @@
+/*
+
+    Propriedades e Métodos do objeto Math
+
+*/
+
+//PROPRIEDADES:
+console.log("Constante de Euler:",Math.E); //Retorna Constante de Euler
+console.log("Logaritmo Natural de 2:",Math.LN2); //Retorna Logaritmo Natural de 2
+console.log("Logaritmo Natural de 10:",Math.LN10); //Retorna Logaritmo Natural de 10
+console.log("Logaritmo E na base 2:",Math.LOG2E); //Retorna Logaritmo de E na base 2
+console.log("Logaritmo E na base 10:",Math.LOG10E); //Retorna Logaritmo de E na base 10
+console.log("Pi:",Math.PI); //Retorna o valor de PI
+console.log("Raiz Quadrada de 1/2:",Math.SQRT1_2); //Retorna a Raiz Quadrada de 1/2
+console.log("Raiz Quadrada de 2:",Math.SQRT2); //Retorna a Raiz Quadrada de 2
+
+//METODOS:
