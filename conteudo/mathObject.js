cf1293b40f0dd42976af94eb7bd78cd908383184
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2022-03-01 15:30:19 -0300

    adiciona metodos principais do objeto Math

--- conteudo/mathObject.js
+++ conteudo/mathObject.js
@@ -14,4 +14,23 @@ console.log("Pi:",Math.PI); //Retorna o valor de PI
 console.log("Raiz Quadrada de 1/2:",Math.SQRT1_2); //Retorna a Raiz Quadrada de 1/2
 console.log("Raiz Quadrada de 2:",Math.SQRT2); //Retorna a Raiz Quadrada de 2
 
-//METODOS:
+//PRINCIPAIS METODOS:
+console.log("Valor absoluto de -2:",Math.abs(-2)); //Retorna o módulo, ou valor absoluto, de um número
+console.log("Menor inteiro que é maior ou igual ao número 4.5:",Math.ceil(4.5)); //Arredonda o número para cima
+console.log("Maior inteiro que é menor ou igual ao número 4.5:",Math.floor(4.5)); //Arredonda o número para baixo
+console.log("Cosseno de 45:",Math.cos(45)); //Retorna o cosseno de um número
+console.log("Seno de 45:",Math.sin(45)); //Retorna o seno de um número
+console.log("Tangente de 45:",Math.tan(45)); //Retorna a tangente de um número
+console.log("Constante de Euler elevado a 4:",Math.exp(4)); //Retorna o valor de e elevado a um numero
+console.log("Logaritmo Natural do número 4:",Math.log(4)); //Retorna o logaritmo natural de um número
+console.log("Logaritmo na Base 10 do número 4:",Math.log10(4)); //Retorna o logaritmo na base 10 de um número
+console.log("Logaritmo na Base 2 do número 4:",Math.log2(4)); //Retorna o logaritmo na base 2 de um número
+console.log("Maior número entre 1,5 e 8:",Math.max(1,5,8)); //Retorna o maior Número entre os parametros
+console.log("Menor número entre 1,5 e 8:",Math.min(1,5,8)); //Retorna o menor Número entre os parametros
+console.log("10 elevado a 2:",Math.pow(10,2)); //Retorna o primeiro parametro elevado ao segundo
+console.log("Número pseudo-aleatório entre 0 e 1:",Math.random()); //Retorna um número pseudo-aleatório entre 0 e 1
+console.log("Arredondamento do número 4.4:",Math.round(4.4)); //Arredonda o número para o inteiro mais próximo
+console.log("O número é -4 é negativo, positivo ou neutro:",Math.sign(-4)); //Retorna o sinal do número
+console.log("A raiz quadrada de 25:",Math.sqrt(25)); //Retorna a raiz quadrada de um número
+console.log("A raiz cubica de 8:",Math.cbrt(8)); //Retorna a raiz cubica de um número
+console.log("A parte inteira de 20.56:",Math.trunc(20.56)); //Retorna a parte inteira do número
