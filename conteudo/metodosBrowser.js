900f7eba98d650d7da28e9f581336f68ca8e71dd
Maycon Douglas <65413165+Maycdouglas@users.noreply.github.com>
2023-02-11 21:07:19 -0300

    Atualiza os metodos Browser

--- conteudo/metodosBrowser.js
+++ conteudo/metodosBrowser.js
@@ -1,2 +1,5 @@
 alert("Olá Maycon") //Insere mensagem de alerta ao abrir a página
+let resultado = 10
+alert(`O resultado da sua conta foi: ${resultado}`) //forma de se usar uma variável dentro do alert
 prompt("Digite um número") //Insere um input para digitar algo ao abrir a página
+confirm("Confirma que deseja apagar?") //Insere um pop-up na tela com opção OK e Cancelar. Retorna True ou False
