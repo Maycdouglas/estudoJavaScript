d7f57e437c61bce3464355f583aa25cdd64dae51
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2024-07-25 20:29:48 -0300

    Adiciona primeira versão do exercicio de validação de CPF

--- exercicios/validateCPF.js
+++ exercicios/validateCPF.js
@@ -15,3 +15,50 @@ Se o resultado for maior do que 9, considera 0
 11 - (284 % 11) = 2 (Segundo dígito)
 
 */
+
+// ----------- FUNÇÕES -----------
+function multiplicaDigitosCPF(digito,indice,cpfArray){
+    digito = Number(digito)
+    let multiplicador = cpfArray.length - indice + 1
+    digito = digito * multiplicador
+    return digito
+}
+
+function somaProdutosCPF(acumulador, atual){
+    acumulador = acumulador + atual
+    return acumulador
+}
+
+function geraDigito(cpfArray){
+    digitsMultiplied = cpfArray.map(multiplicaDigitosCPF)
+    sumDigitsMultiplied = digitsMultiplied.reduce(somaProdutosCPF)
+    
+    digit = 11 - (sumDigitsMultiplied % 11)
+    
+    if(digit > 9)
+        digit = 0
+    
+    return digit
+}
+// -------------------------------
+
+
+//posso transformar o cpf em objeto
+
+let cpf = '705.484.450-52'
+let cpfDigits = cpf.replace(/\D+/g,"")
+let cpfArray = Array.from(cpfDigits)
+let newCPFArray = cpfArray.splice(0,9) 
+
+newCPFArray.push(geraDigito(newCPFArray))
+newCPFArray.push(geraDigito(newCPFArray))
+
+let newCPFDigits = newCPFArray.join("")
+
+if(cpfDigits === newCPFDigits)
+{
+    console.log("CPF VALIDADO COM SUCESSO!")
+} else {
+    console.log("CPF NÃO VALIDADO!")
+}
+    
