0a535b01a6a1ca3806614b5c2e64763089548a5e
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2024-07-26 16:10:02 -0300

    Insere estrutura de dados MAP

---
+++ conteudo/map.js
@@ -0,0 +1,17 @@
+// MAP é uma estrutura de dados
+
+const pessoas = [
+    {id: 3, nome: 'Luiz'},
+    {id: 2, nome: 'Maria'},
+    {id: 1, nome: 'Helena'}
+]
+
+const novasPessoas = new Map()
+
+for (const pessoa of pessoas) {
+    const {id} = pessoa
+    novasPessoas.set(id, {...pessoa})
+}
+
+
+console.log(novasPessoas)
