7e3ea7d38614ba9337e97e41ec4c0b246606f708
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2024-09-29 20:45:09 -0300

    Cria arquivo sobre o tipo de dados SET (conjuntos)

---
+++ conteudo/set.js
@@ -0,0 +1,4 @@
+// SET(conjunto) é uma estrutura semelhante ao array, mas com métodos diferentes e com uma importante regra:
+// elementos não podem se repetir
+
+// link para documentação: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Set
