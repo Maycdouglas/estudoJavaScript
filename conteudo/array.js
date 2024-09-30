884c6d64bfceb44933f63bf1da093a7686930561
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2024-09-30 20:11:14 -0300

    Inicia reformulação do array.js, listando todos os métodos

--- conteudo/array.js
+++ conteudo/array.js
@@ -6,43 +6,224 @@
 
     é valor por referência, ou seja, se uma nova constante receber o mesmo array, tudo que fizer nela, altera o outro e vice-versa
 
+    link documentação: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 */
 
-//Exemplo de Array heterogêneo (ou seja, com elementos de diferentes tipos)
+// Exemplo de Array heterogêneo (ou seja, com elementos de diferentes tipos)
 console.log([
     "Maycon", 
     21, 
     true
 ])
 
-
-//Criando um array
+// Criação de Array
 const animals = [
     'Lion',
     'Monkey',
     'Dog'
 ]
 
-// Criar Array com construtor
-let myArray = new Array('a', 'b', 'c')
-console.log(myArray)
-
-//Forma não muito recomendada para criar array
+// Criação de Array com construtor - Não recomendada
 let aprovados = new Array('Bia', 'Carlos', 'Ana')
-
-//Forma recomendada
-aprovados = ['Bia', 'Carlos', 'Ana']
 console.log(aprovados)
-aprovados.sort() //ordena os elementos do array, altera o array original
-console.log(aprovados)
+const arrayTamanhoSete = new Array(7) // Cria um Array vazio com tamanho 7
+console.log(arrayTamanhoSete)
 
+// Exclusão de elemento - A posição se torna UNDEFINED
 delete aprovados[1] //dessa forma exclui o elemento e o seta como undefined naquela posição
+console.log(aprovados)
 
-//Acessar os valores dentro do Array
+// Acesso dos valores dentro do Array
 console.log(animals[0])
 
-//Tamanho do array
+// ==================== PROPRIEDADES DO ARRAY ==================== //
+
+// Array.length - Tamanho do Array
 console.log(animals.length)
+
+aprovados.sort() //ordena os elementos do array, altera o array original
+
+// ==================== MÉTODOS DO ARRAY ==================== //
+
+/*
+    LISTA DE MÉTODOS
+        - Array.from()
+        - Array.isArray()
+        - Array.of()
+        - Array.prototype.at()
+        - Array.prototype.concat()
+        - Array.prototype.copyWithin()
+        - Array.prototype.entries()
+        - Array.prototype.every()
+        - Array.prototype.fill()
+        - Array.prototype.filter()
+        - Array.prototype.find()
+        - Array.prototype.findIndex()
+        - Array.prototype.findLast()
+        - Array.prototype.findLastIndex()
+        - Array.prototype.flat()
+        - Array.prototype.flatMap()
+        - Array.prototype.forEach()
+        - Array.prototype.includes()
+        - Array.prototype.indexOf()
+        - Array.prototype.join()
+        - Array.prototype.keys()
+        - Array.prototype.lastIndexOf()
+        - Array.prototype.map()
+        - Array.prototype.pop()
+        - Array.prototype.push()
+        - Array.prototype.reduce()
+        - Array.prototype.reduceRight()
+        - Array.prototype.reverse()
+        - Array.prototype.shift()
+        - Array.prototype.slice()
+        - Array.prototype.some()
+        - Array.prototype.sort()
+        - Array.prototype.splice()
+        - Array.prototype.toLocalString()
+        - Array.prototype.toReversed()
+        - Array.prototype.toSorted()
+        - Array.prototype.toSpliced()
+        - Array.prototype.toString()
+        - Array.prototype.unshift()
+        - Array.prototype.values()
+        - Array.prototype.with()
+*/
+
+/* Array.from() 
+    - Cria uma nova instância de Array a partir de um objeto semelhante ou iterável
+    - UTILIDADES:
+        - Converte string em um array de caracteres
+        - Converte um NodeList em um array
+        - Gera um array de números sequenciais
+    - PARAMETROS:
+        - arrayLike - OBRIGATÓRIO
+        - função map - OPCIONAL
+        - thisArg - valor a ser utilizado como this quando a função map for chamada - OPCIONAL
+    - Método estático
+*/
+
+// Conversão de string em um Array de caracteres
+const str = 'OpenAI';
+const array = Array.from(str);
+console.log(array); // ['O', 'p', 'e', 'n', 'A', 'I']
+
+// Gera um array de números sequenciais
+const sequence = Array.from({ length: 10 }, (_, i) => i + 1);
+console.log(sequence); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// =============================== //
+
+/* Array.isArray() 
+    - Verifica se um objeto é uma Array. Retorna TRUE caso seja e FALSE caso não seja
+    - UTILIDADES:
+        - Converte string em um array de caracteres
+        - Converte um NodeList em um array
+        - Gera um array de números sequenciais
+    - PARAMETROS:
+        - objeto - OBRIGATÓRIO
+    - Método estático
+*/
+
+const obj = {
+    name: 'OpenAI',
+    data: [1, 2, 3],
+    meta: {
+      tags: ['AI', 'Machine Learning'],
+      stats: { views: 1000 }
+    }
+  };
+
+console.log("obj.name = ",Array.isArray(obj.name)); // false
+console.log("obj.data = ",Array.isArray(obj.data)); // true
+console.log("obj.meta.tags = ",Array.isArray(obj.meta.tags)); // true
+console.log("obj.meta.stats = ",Array.isArray(obj.meta.stats)); // false
+  
+// =============================== //
+
+/* Array.of() 
+    - Cria um array a partir de uma lista de valores individuais
+    - PARAMETROS:
+        - elementos - podem ser quantos quiser - OBRIGATÓRIO
+    - Método estático
+*/
+const value1 = 42;
+const value2 = 'Hello';
+const value3 = { key: 'value' };
+
+const arrayOf = Array.of(value1, value2, value3);
+console.log(arrayOf); // [42, 'Hello', { key: 'value' }]
+
+// =============================== //
+
+/* Array.prototype.at() 
+    - Permite acessar elementos de um array usando índices positivos e negativos.
+    - Caso utilize índices negativos, será contado do último elemento para trás
+    - UTILIDADES:
+        - Acessa o último elemento de um array
+    - PARAMETROS:
+        - indice - pode ser um número positivo ou negativo - OBRIGATÓRIO
+*/
+// Acessa elemento de um array usando índice
+const arrPositivo = [10, 20, 30, 40];
+console.log(arrPositivo.at(0)); // 10
+
+// Acessa o último elemento de um array usando número negativo
+const arrNegativo = [10, 20, 30, 40];
+console.log(arrNegativo.at(-1)); // 40
+
+// =============================== //
+
+/* Array.prototype.concat() 
+    - Mescla dois ou mais arrays
+    - Retorna um novo array
+    - UTILIDADES:
+        - Concatena arrays e valores
+    - PARAMETROS:
+        - valor - OBRIGATÓRIO
+*/
+
+// Concatena múltiplos arrays
+const arr1 = [1];
+const arr2 = [2];
+const arr3 = [3, 4];
+const resultMultiplos = arr1.concat(arr2, arr3);
+console.log(resultMultiplos); // [1, 2, 3, 4]
+
+// Concatena arrays e valores
+const arr = [1, 2];
+const result = arr.concat(3, 4);
+console.log(result); // [1, 2, 3, 4]
+
+// =============================== //
+
+/* Array.prototype.copyWithin() 
+    - 
+    - UTILIDADES:
+        - 
+    - PARAMETROS:
+        - 
+*/
+
+
+
+// =============================== //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ============================================================ //
 
