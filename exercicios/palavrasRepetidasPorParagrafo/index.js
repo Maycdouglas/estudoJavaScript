18baed6493782fedfec396b90e025c7ad47afb50
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2024-10-01 19:00:53 -0300

    insere arquivo inicial do exercicio de palavras duplicadas em paragrafos

---
+++ exercicios/palavrasRepetidasPorParagrafo/index.js
@@ -0,0 +1,7 @@
+/*
+    - Este é um exercício realizado no curso JavaScript com Node.js: criando sua primeira biblioteca
+    - Objetivo é criar uma aplicação capaz de retornar palavras repetidas em cada parágrafo de um texto, a fim de identificar quando deve substituir para um sinônimo e deixar o texto menos repetitivo.
+
+
+
+*/
